19ea015366762fe666355f04773a693be1c03e5f
Dmitry Belov <Dmitry.Belov2@airbank.cz>
2024-06-02 00:15:19 +0200

    add error page and loading

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const ErrorPage = () => {
+  return (
+    <div className="text-white font-bold p-5">
+      <h1>Ooop something went wrong...</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import { default as CustomLoading } from '@/components/commons/Loading'
+
+const Loading = () => {
+  return <CustomLoading />
+}
+
+export default Loading
